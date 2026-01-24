83026773715909e373afea267f0a5bcccd5d2a5f
Amina <abilmazhinova07@mail.ru>
2026-01-24 22:54:39 +0500

    5th task

---
+++ numbers/5.py
@@ -0,0 +1,12 @@
+x=1 #int
+y=2.8 #float
+z=1j #complex
+a=float(x) #convert from int to float
+b=int(y) #convert from float to int
+c=complex(x) #convert from int to complex
+print(a) #1.0
+print(b) #2
+print(c) #(1+0j)
+print(type(a)) #float
+print(type(b)) #int
+print(type(c)) #complex
