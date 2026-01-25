5d7324c3f771d185927f66f999076eb28819ed89
Amina <abilmazhinova07@mail.ru>
2026-01-25 11:03:10 +0500

    27th task

---
+++ strings/27.py
@@ -0,0 +1,11 @@
+txt="Amina is the \rbest" #besta is the
+print(txt)
+#Amina is the
+#best
+#курсор А ға барады, потом best деген Amin дегенді жауып тастайды қалғандары сол беті қалады
+
+txt="Hello\rWorld!" #World!
+print(txt)
+#Hello
+#World!
+#курсор Hello дегеннің басына барады, World деген Hello дегенді жауып тастайды қалғандары сол беті қалады
