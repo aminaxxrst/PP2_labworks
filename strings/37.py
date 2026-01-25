49df2c9bb42573977b733f3567156427a6bb61ac
Amina <abilmazhinova07@mail.ru>
2026-01-25 16:43:09 +0500

    37th task

---
+++ strings/37.py
@@ -0,0 +1,6 @@
+#join()- тізімнің ішіндегі элеметтерді бір мәтінге бріктіру
+people=["Amina", "Kamila", "Symbat"]
+print(" ".join(people)) #"Amina Kamila Symbat"
+print(", ".join(people)) #"Amina, Kamila, Symbat"
+print("-".join(people)) #"Amina-Kamila-Symbat"
+print("".join(people)) #AminaKamilaSymbat
