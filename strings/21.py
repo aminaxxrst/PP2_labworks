108c5ccd6daed3d05f587087c0702128fecaa32e
Amina <abilmazhinova07@mail.ru>
2026-01-25 10:15:24 +0500

    21st task

--- strings/21.py
+++ strings/21.py
@@ -1,5 +1,5 @@
 #placeholder can include a modifier to format the value
 #a modifier is included by adding a colon (:) followed by a legal formatting type, like .2f which means fixed point number with 2 decimals
 price=59
-txt=f"The price is {price:.2f} dollar" #59.00
+txt=f"The price is {price:.2f} dollar " #59.00
 print(txt)
