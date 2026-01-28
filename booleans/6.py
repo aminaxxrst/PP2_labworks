dfce673a86f6278ee8e54269dbea4aa0a6951e17
Amina <abilmazhinova07@mail.ru>
2026-01-28 22:40:19 +0500

    6th task

---
+++ booleans/6.py
@@ -0,0 +1,8 @@
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool('')) #empty string
+print(bool(())) #empty tuple
+print(bool([])) #empty list
+print(bool({})) #empty dictionary
+#answers: False
