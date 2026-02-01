2f7fe346912c6ba7f36464fa65e17de2a2d41cd2
Amina <abilmazhinova07@mail.ru>
2026-02-01 21:24:15 +0500

    24th task

---
+++ operators/24.py
@@ -0,0 +1,7 @@
+print(6|3) #7 (0111)
+'''
+| operator compares each bit and set it to 1 if one or both is 1,otherwise it is set to 0
+6 -> 0110
+3 -> 0011
+6|3 -> 0111 -> 1*2^0+1*2^1+1*2^2=1+2+4=7
+'''
