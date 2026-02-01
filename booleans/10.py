1a605b0f9c620109b2cc776f5c0ecfa18ab60959
Amina <abilmazhinova07@mail.ru>
2026-02-01 19:37:18 +0500

    10th task

---
+++ booleans/10.py
@@ -0,0 +1,3 @@
+#Python also has many built-in functions that return a boolean value, as isinstance() function which can be used to determine if an object of a certain data type
+x=200
+print(isinstance(x, int)) #True
