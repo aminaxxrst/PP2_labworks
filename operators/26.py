f765208b0c22db7a889ed43fa393bde0c49cfa45
Amina <abilmazhinova07@mail.ru>
2026-02-01 21:30:24 +0500

    26th task

---
+++ operators/26.py
@@ -0,0 +1,5 @@
+'''
+~ inverts all the bits
+<< shift left by pushing zeros in from the right and let the leftmost bits fall off
+>> shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+'''
