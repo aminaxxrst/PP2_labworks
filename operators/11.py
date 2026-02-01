f79a283f6d5a6a329a835b3b9b5818eae16b8348
Amina <abilmazhinova07@mail.ru>
2026-02-01 20:41:18 +0500

    11th task

---
+++ operators/11.py
@@ -0,0 +1,5 @@
+#logical operators- used to combine conditional statements
+x=int(input())
+print(x<5 and x<10) #Returns True if both statements are true
+print(x<5 or x<4) #Returns True if one of the statements is true
+print(not(x<5 and x<10)) #Returns the result, returns False if the result is true
