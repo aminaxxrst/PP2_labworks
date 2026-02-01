61739d95d1051b564e0f19896c922b3ffe5dd262
Amina <abilmazhinova07@mail.ru>
2026-02-01 23:28:13 +0500

    21st task

---
+++ if_else/21.py
@@ -0,0 +1,6 @@
+#and- logical operation, to combine conditional statements, both conditions must be true for the entire expression to be true
+a=200
+b=33
+c=500
+if a>b and c>a:
+    print("Both conditions are true") #Both conditions are true
