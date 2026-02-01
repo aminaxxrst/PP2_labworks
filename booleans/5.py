ed333459b3278dbfcec551d8f7200151547767b2
Amina <abilmazhinova07@mail.ru>
2026-02-01 19:23:57 +0500

    5th task

--- booleans/5.py
+++ booleans/5.py
@@ -1,4 +1,4 @@
-#bool()- returns False for '', 0, empt set, tuple, list and dictionary
+#bool()- returns False for '', 0, empty set, tuple, list and dictionary
 print(bool("abc")) #True
 print(bool(123)) #True
 print(bool(['apple', 'cherry', 'banana'])) #True
