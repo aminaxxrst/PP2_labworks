f07b2236369f420644063cb04b29ee5cb877a0bb
Amina <abilmazhinova07@mail.ru>
2026-02-01 21:57:12 +0500

    5th task

---
+++ if_else/5.py
@@ -0,0 +1,5 @@
+#boolean variables can be used directly in if statements without comparison operators
+is_logged_in=True
+if is_logged_in:
+    print("Welcome beck!")
+#s="False", bool(s)=True because it is non-empty string
