2d6b4bcad40d4f0c0a638d6cdb2d9c2652771077
Amina <abilmazhinova07@mail.ru>
2026-02-01 22:10:51 +0500

    9th task

---
+++ if_else/9.py
@@ -0,0 +1,16 @@
+#use elif rather than if because if it finds one true condition, it stops checking other
+day=3
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday") #Wednesday
+elif day==4:
+    print("Thursday")
+elif day==5:
+    print("Friday")
+elif day==6:
+    print("Saturday")
+elif day==7:
+    print("Sunday")
