783895c13467b3bd2b89b35281e38bc2af0247ab
Amina <abilmazhinova07@mail.ru>
2026-02-01 19:34:37 +0500

    9th task

---
+++ booleans/9.py
@@ -0,0 +1,9 @@
+#you can execute code based on the boolean answer of a function
+#print "YES!" if the function returns True, otherwise print "NO!"
+def myFunction():
+    return True
+
+if myFunction():
+    print("YES!") #YES!
+else: 
+    print("NO!")
