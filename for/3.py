b9a243423e9f7143b18e36f51a57a198c7fbf56a
Amina <abilmazhinova07@mail.ru>
2026-02-01 23:00:38 +0500

    3rd task

---
+++ for/3.py
@@ -0,0 +1,7 @@
+#break- stop the loop before it has looped through all the items
+fruits=["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x=="banana":
+        break
+#apple banana
