7fc3af8cf514e87f8cedddef7d4b53a156d46ba5
Amina <abilmazhinova07@mail.ru>
2026-02-01 21:26:37 +0500

    25th task

---
+++ operators/25.py
@@ -0,0 +1,7 @@
+print(6^3) #5(0101)
+''''
+^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0
+6 -> 0110
+3 -> 0011
+6^3 -> 0101 -> 1*2^0+1*2^2=1+4=5
+'''
