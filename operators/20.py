047073f798c14386eeaa527067da3bd0a3a57ebe
Amina <abilmazhinova07@mail.ru>
2026-02-01 21:13:10 +0500

    20th task

---
+++ operators/20.py
@@ -0,0 +1,4 @@
+#membership operators- used to test if a sequence is presented in an object
+fruits= ["apple", "banana", "cherry"]
+print("banana" in fruits) #True
+#in returns True if a sequence with the specified value is present in the object
