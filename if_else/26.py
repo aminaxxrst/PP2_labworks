1810dc7d07e066b0917d9600c5503994e456bd86
Amina <abilmazhinova07@mail.ru>
2026-02-01 23:42:59 +0500

    26th task

---
+++ if_else/26.py
@@ -0,0 +1,10 @@
+username="Tobias"
+password="secret123"
+is_verified=True
+if username and password and is_verified:
+    print("Login successful")
+else:
+    print("Login failed") #Login successful
+'''
+True and True and True=True
+'''
