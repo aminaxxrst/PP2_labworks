605ba5e30460480dd5890ea37b8a902a219e727e
Amina <abilmazhinova07@mail.ru>
2026-02-01 22:43:42 +0500

    1st task

---
+++ while/1.py
@@ -0,0 +1,7 @@
+#while- can execute a set of statements as long as a condition is true
+i=1
+while i<6:
+    print(i)
+    i+=1 #1 2 3 4 5
+#remember to increment i, or else the loop will continue forever
+#while loop requires relevant variables to be ready, in this situation we have to define i before the while loop
