142b1a81d33c45f9ee550780d41a7fc755649a79
Amina <abilmazhinova07@mail.ru>
2026-02-01 23:40:34 +0500

    25th task

---
+++ if_else/25.py
@@ -0,0 +1,10 @@
+#using parentheses- make your code clear and control the order of evaluation
+temperature=25
+is_raining=False
+is_weekend=True
+if (temperature>20 and not is_raining) or is_weekend:
+    print("Great day for outdoor activities!") #Great day for outdoor activities
+'''
+True and True= True
+True or True= True
+'''
