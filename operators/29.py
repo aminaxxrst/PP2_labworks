702895ba770870cccb9f44ca6442fa3dabf4f8a9
Amina <abilmazhinova07@mail.ru>
2026-02-01 21:42:11 +0500

    29th task

---
+++ operators/29.py
@@ -0,0 +1,16 @@
+'''
+precedence order:
+()
+**
++x, -x, ~x (unary plus, unary minus, bitwise not)
+*, /, //, % 
++, -
+>>, << (bitwise right and left shifts)
+& (bitwise and)
+^ (bitwise xor)
+| (bitwise or)
+==, !=, >, >=, <, <=, is, is not, in, not in (comparisons, identity and membership operators)
+not (logical not)
+and
+or
+'''
