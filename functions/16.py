06786d81e3d12ae8ac1a9edffa3436025d6d0e45
Amina <abilmazhinova07@mail.ru>
2026-02-07 19:56:31 +0500

    16th task

---
+++ functions/16.py
@@ -0,0 +1,6 @@
+#positional arguments(there is no arguments as variable=value)
+def my_function(animal, name):
+    print("I have a", animal) #I have a dog
+    print("My", animal + "'s name is", name) #My dog's name is Buddy
+
+my_function("dog", "Buddy")
