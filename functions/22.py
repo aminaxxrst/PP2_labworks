6e0ce1bf7957f61a8243729509d14f218aa9100a
Amina <abilmazhinova07@mail.ru>
2026-02-07 20:15:51 +0500

    22nd task

---
+++ functions/22.py
@@ -0,0 +1,8 @@
+#functions can return any data type (list, dictionary, string, number)
+def my_function():
+    return ["apple", "banana", "cherry"]
+
+fruits=my_function()
+print(fruits[0]) #apple
+print(fruits[1]) #banana
+print(fruits[2]) #cherry
