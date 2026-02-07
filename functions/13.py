9ca2fefeeecbeee72cc8c88889d7f816ce007c80
Amina <abilmazhinova07@mail.ru>
2026-02-07 19:47:45 +0500

    13th task

---
+++ functions/13.py
@@ -0,0 +1,6 @@
+def my_function(country="Norway"):
+    print("I am from", country)
+my_function("Sweden") #I am from Sweden
+my_function("India") #I am from India
+my_function() #I am from Norway
+my_function("Brazil") #I am from Brazil
