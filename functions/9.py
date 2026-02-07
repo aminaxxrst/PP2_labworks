7c5817b0fef1b097c2a5eb7a60ac687087f9b270
Amina <abilmazhinova07@mail.ru>
2026-02-07 19:29:38 +0500

    9th task

---
+++ functions/9.py
@@ -0,0 +1,7 @@
+#arguments- variables inside a bracket next to function_name
+def my_function(fname):
+    print(fname+" Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
