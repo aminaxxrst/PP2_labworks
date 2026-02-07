29199c4cac505b01465fed9ca2d19074d776d61f
Amina <abilmazhinova07@mail.ru>
2026-02-07 21:45:45 +0500

    34th task

---
+++ functions/34.py
@@ -0,0 +1,6 @@
+#**kwargs take those parameters as a dictionary
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+#His last name is Refsnes
