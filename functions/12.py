68993946975ba8106b0a4491dbf8f5972bc70eb6
Amina <abilmazhinova07@mail.ru>
2026-02-07 19:38:50 +0500

    12th task

---
+++ functions/12.py
@@ -0,0 +1,7 @@
+def my_function(name="friend"):
+    print("Hello", name)
+
+my_function("Emil") #Hello Emil
+my_function("Tobias") #Hello Tobias
+my_function() #Hello friend
+my_function("Linus") #Hello Linus
