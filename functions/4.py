0987da7ae07be9506f748b9ede1e4afd95255831
Amina <abilmazhinova07@mail.ru>
2026-02-07 19:19:45 +0500

    4th task

---
+++ functions/4.py
@@ -0,0 +1,10 @@
+def calculate_sum():
+    pass
+def _private_function():
+    pass
+def myFunction2():
+    pass
+
+calculate_sum() #function name can only contain letters, numbers and underscores
+_private_function() #function name must start with a letter/underscore
+myFunction2() #fuction name is case-sensitive (amina doesn't equal to Amina)
