66d1ca663e2df1b8c22c8b366d33368c25e6ee25
Amina <abilmazhinova07@mail.ru>
2026-02-07 21:40:22 +0500

    31st task

---
+++ functions/31.py
@@ -0,0 +1,9 @@
+#Regular arguments with *args
+def my_function(greeting, *names):
+  for name in names:
+    print(greeting, name) 
+    #Hello Emil
+    #Hello Tobias
+    #Hello Linus
+
+my_function("Hello", "Emil", "Tobias", "Linus")
