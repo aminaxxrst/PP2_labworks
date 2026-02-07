c38464830ecad57d2f94046e9661eb67af058536
Amina <abilmazhinova07@mail.ru>
2026-02-07 18:49:28 +0500

    35th task

---
+++ if_else/35.py
@@ -0,0 +1,6 @@
+#if statements cannot be empty, for that reason if your "if statement" doesn't have any context just write "pass"
+#pass- null operation, nothing happens when it executes (as a placeholder)
+a=33
+b=200
+if b>a:
+    pass
