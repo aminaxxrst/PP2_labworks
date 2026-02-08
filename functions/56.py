16676ffdb2b0fb5b1a50036525486c3d9590f375
Amina <abilmazhinova07@mail.ru>
2026-02-08 10:56:19 +0500

    56th task

---
+++ functions/56.py
@@ -0,0 +1,14 @@
+#Import functools.wraps to preserve the original function name
+import functools
+
+def changecase(func):
+  @functools.wraps(func)
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Have a great day!"
+
+print(myfunction.__name__) #myfunction
