eb1567f39b2e920b61be9e57cb59b214ef4227e9
Amina <abilmazhinova07@mail.ru>
2026-02-08 12:42:17 +0500

    82nd task

---
+++ functions/82.py
@@ -0,0 +1,10 @@
+#send() method allows you to send a value to the generator
+def echo_generator():
+  while True:
+    received = yield
+    print("Received:", received)
+
+gen = echo_generator()
+next(gen) # Prime the generator
+gen.send("Hello") #Received: Hello
+gen.send("World") #Received: World
