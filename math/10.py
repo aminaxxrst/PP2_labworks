a6ad6bd935b1be274b45abb6d0bd5dc609c6b523
Amina <abilmazhinova07@mail.ru>
2026-02-23 01:37:05 +0500

    10th task

---
+++ math/10.py
@@ -0,0 +1,8 @@
+# Print the lowest value
+print(min(5, 10))
+# Print the highest value
+print(max(5, 10))
+# Print the absolute value
+print(abs(-7.25))
+# Print 4 to the power of 3
+print(pow(4, 3))
