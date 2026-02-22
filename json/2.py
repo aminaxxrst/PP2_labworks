c229344a482dd76a2b0bd9ca0cf913d601446c60
Amina <abilmazhinova07@mail.ru>
2026-02-23 01:40:20 +0500

    12th task

---
+++ json/2.py
@@ -0,0 +1,16 @@
+#from python to json
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+#{"name": "John", "age": 30, "city": "New York"}
