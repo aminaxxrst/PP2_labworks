f34f0752a46ff66dc3ec1fb13c073f270f9ae556
Amina <abilmazhinova07@mail.ru>
2026-02-23 01:43:09 +0500

    3rd task

---
+++ json/3.py
@@ -0,0 +1,11 @@
+import json
+
+print(json.dumps({"name": "John", "age": 30})) #{"name": "John", "age": 30}
+print(json.dumps(["apple", "bananas"])) #["apple", "bananas"]
+print(json.dumps(("apple", "bananas"))) #["apple", "bananas"]
+print(json.dumps("hello")) #"hello"
+print(json.dumps(42)) #42
+print(json.dumps(31.76)) #31.76
+print(json.dumps(True)) #true
+print(json.dumps(False)) #false
+print(json.dumps(None)) #null
