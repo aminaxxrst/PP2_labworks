4dc7651cbf59708abc7005beab9c422b403cac9e
Amina <abilmazhinova07@mail.ru>
2026-02-23 01:53:53 +0500

    7th task

---
+++ json/7.py
@@ -0,0 +1,41 @@
+#Use the sort_keys parameter to specify if the result should be sorted or not
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4, sort_keys=True))
+
+'''
+{
+    "age": 30,
+    "cars": [
+        {
+            "model": "BMW 230",
+            "mpg": 27.5
+        },
+        {
+            "model": "Ford Edge",
+            "mpg": 24.1
+        }
+    ],
+    "children": [
+        "Ann",
+        "Billy"
+    ],
+    "divorced": false,
+    "married": true,
+    "name": "John",
+    "pets": null
+}
+'''
